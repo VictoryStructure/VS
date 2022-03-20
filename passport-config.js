b8e3883fb3040a3c7409fe5f3abcac45ef4e8c84
Stacy Nguyen <thaiha2905@gmail.com>
2022-03-20 02:20:49 +0000

    add paths to server.js and read from json

--- passport-config.js
+++ passport-config.js
@@ -1,1 +1,34 @@
-// function initialize(passport)
+// referenced from https://github.com/WebDevSimplified/Nodejs-Passport-Login/blob/master/passport-config.js
+
+const LocalStrategy = require('passport-local').Strategy
+
+function initialize(passport, getUsername, getUserId) {
+    const authenticateUser = async (username, password, done) => {
+        const user = getUsername(username)
+        
+        if (user == null) {
+            return done(null, false, {message: 'Username not recognized'})
+        }
+
+        try {
+            if (password == user.password) {
+                return done(null, user)
+            } 
+            else {
+                return done(null, false, {message: 'Password incorrect'})
+            }
+        } 
+        
+        catch (e) {
+            return done(e)
+        }
+    }
+
+    passport.use(new LocalStrategy({usernameField: 'username'}, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserId(id))
+    })
+}
+
+module.exports = initialize
--- server.js
+++ server.js
@@ -1,25 +1,74 @@
+// server.js
+//
+// Right now only contains login system
+// Referenced from https://github.com/WebDevSimplified/Nodejs-Passport-Login/blob/master/server.js
+
 const express = require('express')
 const app = express()
-const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
-// const users = [{
-//     name: 'John Doe',
-//     email: 'john.doe@uea.ac.uk',
-//     password: 'john'
-// }]
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  username => users.find(user => user.username === username),
+  id => users.find(user => user.id === id)
+)
 
 app.set('view-engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: false})) // https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
+app.use(flash())
 
-// https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
-app.use(express.urlencoded({extended: false}))
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
 
-app.get('/login', (req, res) => {
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+// read json data
+const fs = require('fs');
+let rawdata = fs.readFileSync('users.json');
+let users = JSON.parse(rawdata);
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.username })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('LoginVS.ejs')
 })
 
-app.post('/login', (req, res) => {
-    // req.body.email -> email corresponds to name field in ejs form
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
 })
+  
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
 
 app.listen(3000)
