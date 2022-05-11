2edc0bf776ac919bce0597be7399cfb30f76a3f4
Francesca Nutt <NuttRoast@gmail.com>
2022-05-11 16:00:33 +0100

    coursework

--- public/js/Coursework.js
+++ public/js/Coursework.js
@@ -1,9 +1,14 @@
 2// referenced from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_rangeslider_round
 
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
 slider.oninput = function() {
-  output.innerHTML = this.value;
+	var output = document.getElementById("demo");
+	output.innerHTML = slider.value;
+	output.innerHTML = this.value;
+}
+
+function handleChange(checkbox) {
+    if(checkbox.checked == true){
+        alert("boop");
+    }
 }
