b3902d6ae4d3305bc5d030cd307e9289397e6115
Rbab96 <98762928+Rbab96@users.noreply.github.com>
2022-05-13 16:49:49 +0100

    Temp Removed Alert

--- public/js/Module.js
+++ public/js/Module.js
@@ -1,12 +1,12 @@
 
-function confirmSubmit() {
-	var text;
-	var r = confirm("Are you sure you want to create the Module? ");
-	if (r == true) {
-		alert("Module created");
+// function confirmSubmit() {
+// 	var text;
+// 	var r = confirm("Are you sure you want to create the Module? ");
+// 	if (r == true) {
+// 		alert("Module created");
 
-	} else {
-		alert("Cancelled Module");
-		event.preventDefault();
-	}
-}
+// 	} else {
+// 		alert("Cancelled Module");
+// 		event.preventDefault();
+// 	}
+// }
